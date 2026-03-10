4c11e25371732f672d92ec693e6b98a5e8553906
adityagpt2004 <adityakumar_2022@vitbhopal.ac.in>
2026-03-10 22:53:45 +0530

    Day 1 - Band Name Generator

---
+++ Day-01-Band-Name-Generator/main.py
@@ -0,0 +1,5 @@
+print("Welcome to the Band Name Generator!")
+city  = input("Enter the city name where you grew up in:\n")
+pet = input("Enter your pet name:\n")
+band_name = city + " " + pet
+print("Your band name could be: ", band_name)
